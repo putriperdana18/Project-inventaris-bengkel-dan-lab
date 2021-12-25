7f12fc700571152a6e45645a39c73b44d7192d96
putriperdana18 <putri_249@smkassalaambandung.sch.id>
2021-12-25 03:51:11 -0800

    mengistal adminLTE

---
+++ resources/views/pengarang/index.blade.php
@@ -0,0 +1,23 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+
+Dashboard
+
+@stop
+
+@section('content')
+
+Halaman Penggarang
+
+@stop
+
+@section('css')
+
+@stop
+
+@section('js')
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -25,35 +25,46 @@ Auth::routes([
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
    // hanya untuk role admin
-Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], function(){
-    Route::get('/', function(){
-        return 'halaman admin';
-    });
+// Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], function(){
+//     Route::get('/', function(){
+//         return 'halaman admin';
+//     });
 
-    Route::get('profile', function(){
-        return 'halaman profile admin';
-    });
-});
+//     Route::get('profile', function(){
+//         return 'halaman profile admin';
+//     });
+// });
 
-// hanya untuk role pengguna atau pengunjung
-Route::group(['prefix' => 'pengguna', 'middleware' => ['auth', 'role:pengguna']], function(){
-    Route::get('/', function(){
-        return 'halaman pengguna';
-    });
+// // hanya untuk role pengguna atau pengunjung
+// Route::group(['prefix' => 'pengguna', 'middleware' => ['auth', 'role:pengguna']], function(){
+//     Route::get('/', function(){
+//         return 'halaman pengguna';
+//     });
 
-    Route::get('profile', function(){
-        return 'halaman profile pengguna';
-    });
-});
+//     Route::get('profile', function(){
+//         return 'halaman profile pengguna';
+//     });
+// });
 
-// hanya untuk role admin atau kasir
-Route::group(['prefix' => 'pembelian', 'middleware' => ['auth', 'role:admin|kasir']], function(){
-    Route::get('/', function(){
-        return 'halaman pembelian';
-    });
+// // hanya untuk role admin atau kasir
+// Route::group(['prefix' => 'pembelian', 'middleware' => ['auth', 'role:admin|kasir']], function(){
+//     Route::get('/', function(){
+//         return 'halaman pembelian';
+//     });
 
-    Route::get('laporan', function(){
-        return 'halaman laporan pengguna';
-    });
+//     Route::get('laporan', function(){
+//         return 'halaman laporan pengguna';
+//     });
+// });
+
+// hanya contoh 
+Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function(){
+    Route::get('buku', function(){
+        return view ('buku.index');
+    })->middleware(['role:admin|pengguna']);
+
+    Route::get('pengarang', function(){
+        return view ('pengarang.index');
+    })->middleware(['role:admin']);
 });
 
